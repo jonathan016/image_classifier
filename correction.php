313bdd60a44000d6b0bf37904525d4237f2730e6
Jonathan <jonathan16998@gmail.com>
2018-07-02 14:57:30 +0700

    Finished creating documentation

--- correction.php
+++ correction.php
@@ -25,9 +25,9 @@
             $increment += 10;
         }
         
-        shell_exec('python -m scripts.retrain --bottleneck_dir=tf_files/bottlenecks --model_dir=tf_files/models/ --summaries_dir=tf_files/training_summaries/"mobilenet_0.50_224" --output_graph=tf_files/retrained_graph.pb --output_labels=tf_files/retrained_labels.txt --architecture="mobilenet_0.50_224" --image_dir=tf_files/training_images');
+        // shell_exec('python -m scripts.retrain --bottleneck_dir=tf_files/bottlenecks --model_dir=tf_files/models/ --summaries_dir=tf_files/training_summaries/"mobilenet_0.50_224" --output_graph=tf_files/retrained_graph.pb --output_labels=tf_files/retrained_labels.txt --architecture="mobilenet_0.50_224" --image_dir=tf_files/training_images --random_crop=15');
         
-        echo "Thank you for your response. Please click <a href=\"index.html\">here</a> to go to index.html.";
+        echo "Thank you for your response. Please retrain the model again, or you may click <a href=\"index.html\">here</a> to go to index.html.";
     } else {
         echo file_get_contents("index.html");
     }
